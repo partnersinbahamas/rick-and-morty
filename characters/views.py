e5ff0e42b25866a40cb1ad58cad74f4f1df577c0
Denys Bokov <denisbokov1703@gmail.com>
2026-03-08 13:52:45 +0100

    feat: create random character api endpoint and search character endpoint

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import HttpRequest
+
+from rest_framework import status, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Character
+from .serializers import CharacterSerializer
+
+
+@api_view(["GET"])
+def get_random_character_view(request: HttpRequest):
+    character_pk_list = Character.objects.values_list("pk", flat=True)
+    random_pk = random.choice(character_pk_list)
+
+    character = Character.objects.get(pk=random_pk)
+
+    serializer = CharacterSerializer(character)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterList(generics.ListAPIView):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        query = self.request.query_params.get("query")
+
+        if not query:
+            raise ValueError("Query parameter is required")
+
+        self.queryset = self.queryset.filter(name__icontains=query)
+
+        return self.queryset
