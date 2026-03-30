26ea6155589ed4399e2e8c6b2234724d65f4168e
Denys Bokov <denisbokov1703@gmail.com>
2026-03-30 18:50:42 +0200

    feat: create celery task for rick and morty api synchronization

--- characters/tasks.py
+++ characters/tasks.py
@@ -1,7 +1,13 @@
 from celery import shared_task
 
+from app.scrapper import sync_characters
+
 from .models import Character
 
 @shared_task
 def characters_count():
     return Character.objects.count()
+
+@shared_task
+def run_characters_sync():
+    return sync_characters()
